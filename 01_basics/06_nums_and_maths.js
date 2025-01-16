185b1948ab6f79c4813abd59dd508c83406df538
Sourav <souravjaryal446@gmail.com>
2025-01-17 00:00:19 +0530

    chanegs

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -23,6 +23,9 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.2));
 console.log(Math.floor(4.8));
+console.log(Math.max(1,2,4,6,8));
+console.log(Math.min(1,2,4,6,8));
+console.log(Math.floor(4.8));
 
 
 
