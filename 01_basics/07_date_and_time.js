7bf38530c1a87c4b139f6b08f683ab7a27a29dd7
Sourav <souravjaryal446@gmail.com>
2025-01-17 00:58:43 +0530

    date and time code update

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,14 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate); // 2025-01-16T19:21:04.311Z
+console.log(myDate.toString()); // Fri Jan 17 2025 00:51:04 GMT+0530 (India Standard Time)
+console.log(myDate.toISOString()); // 2025-01-16T19:21:04.311Z
+console.log(myDate.toJSON()); // 2025-01-16T19:21:04.311Z
+console.log(myDate.toDateString()); // Fri Jan 17 2025
+console.log(myDate.toTimeString()); // 00:51:04 GMT+0530 (India Standard Time)
+console.log(myDate.toLocaleDateString()); // 17/01/2025
+console.log(myDate.toLocaleString()); // 17/01/2025, 00:51:04
+console.log(typeof myDate());
+
+
