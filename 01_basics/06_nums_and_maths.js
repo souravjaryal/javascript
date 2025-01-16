149934b42e68b6f0d35669428142ef62db84b015
Sourav <souravjaryal446@gmail.com>
2025-01-16 23:55:55 +0530

    number and math objects in js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -18,4 +18,12 @@ console.log(hundreds.toLocaleString('en-In'));
 
 
 // Math
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+
+
+
 
